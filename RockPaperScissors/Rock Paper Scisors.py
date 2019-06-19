b3bb4e0f7fa6bbc30c5ff0bf396e66fd5d4adc67
JD <Dobinson_john@yahoo.co.uk>
2019-06-19 20:55:57 +0100

    Functionality added
    User name input now prints label and removes button

--- RockPaperScissors/Rock Paper Scisors.py
+++ RockPaperScissors/Rock Paper Scisors.py
@@ -12,6 +12,7 @@ def endGame():
 
 def startGame():
     print("Lets begin" + userNameIP.get() + ', good luck!')
+    
 
 def userName():
     userNameIP.entry = StringVar()
@@ -19,13 +20,18 @@ def userName():
     userNameIP.get()
 
 def enterUserName():
-    print("Welcome " + userNameIP.get() + ", press Start Game to play.")
+    enterUserName.pack_forget()
+    label = tk.Label(middle_frame, text="Welcome " + userNameIP.get() + ", press Start Game to play.", bg="white")
+    label.pack()
+
+
+
 
 
 # setting up tkinter
 window = tk.Tk()
 window.title("Rock Paper Scissors")
-window.geometry("275x250")
+window.geometry("400x300")
 window.configure(background="black")
 window.iconbitmap("RPS.ico")
 
@@ -34,9 +40,10 @@ path = "RockPaperScissors100x90.png"
 
 # tkinter widgets
 # separated into 2 sections top/middle/bottom
-top_frame = tk.Frame(window, bg="black", width =300, height = 50)
-middle_frame = tk.Frame(window, bg="white", width = 500 )
-bottom_frame = tk.Frame(window, bg="black", width =300, height = 100)
+top_frame = tk.Frame(window, bg="black", width =250, height = 50)
+middle_frame = tk.Frame(window, bg="white", width = 250, height = 250)
+middleBottom_frame = tk.Frame(window, bg="white", width = 350, height = 5)
+bottom_frame = tk.Frame(window, bg="black", width =250, height = 100)
 
 # top frame items
 labelGameName = tk.Label(top_frame, text="Lets play - Rock Paper Scissors", pady = "1", bg="black", fg="white")
@@ -46,7 +53,8 @@ gameImage = ImageTk.PhotoImage(Image.open(path))
 imagePanel = tk.Label(middle_frame, image = gameImage)
 userNameLabel = tk.Label(middle_frame, text= "Please enter your name : ", bg="white", fg="black")
 userNameIP = tk.Entry(middle_frame, justify = "center",)
-enterUserName = tk.Button(middle_frame, text = "Enter user name", command=enterUserName)
+enterUserName = tk.Button(middle_frame, text = "Enter username", command=enterUserName)
+
 
 # bottom frame items
 buttonStart = tk.Button(bottom_frame, text="Start Game", pady = "5", padx = "5")
@@ -55,17 +63,22 @@ buttonQuit = tk.Button(bottom_frame, text="Exit", command= endGame,)
 
 # Packing widgets into tk window
 labelGameName.pack(side = "top", pady = "5")
-imagePanel.pack(fill = "both", expand = "yes", pady = "5" )
+imagePanel.pack(fill = "none", expand = "yes", pady = "5" )
 
 buttonQuit.pack(side = "left")
-buttonStart.pack(side = "bottom")
+buttonStart.pack(side = "bottom", ipadx = "30", padx = "5")
 userNameLabel.pack()
+
+
 enterUserName.pack(side = "bottom", pady = "3")
 userNameIP.pack(side = "bottom", pady = "10",)
 
+
 top_frame.pack(side = "top")
 middle_frame.pack()
-bottom_frame.pack(side = "bottom")
+bottom_frame.pack(side = "bottom", pady = "5")
+middleBottom_frame.pack(side = "bottom")
+
 
 
 # Start the window GUI
